e5f0e183bc44a6a251388c390001120c91ff9132
Harry Galliano <harrygalliano97@gmail.com>
2019-03-01 15:19:23 +0100

    step 7

--- directory.rb
+++ directory.rb
@@ -12,18 +12,31 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
-	def print_header
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+def print_header
   puts "The students of Villains Academy"
   puts "-------------"
+end
+def input_students
+  puts "please enter the names of the students"
+  puts "to finish just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "now we have #{students.count} students"
+    name = gets.chomp
+  end
+students
 end
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
+students = input_students
 print_header
 print(students)
 print_footer(students)
